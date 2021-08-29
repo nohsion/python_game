eac47d2b8b22ffe922bb981d1bb9d8dfe3de01e1
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-29 16:04:44 +0900

    미사일 발사 기능

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class obj:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.move = 0
 
     def put_img(self, addr):
         if addr[-3:] == 'png':
@@ -39,6 +40,14 @@ jet.put_img("./image/jet.png")
 jet.change_size(50, 50)
 jet.x = round((size[0] - jet.sx) / 2)
 jet.y = size[1] - 5 - jet.sy
+jet.move = 5
+
+left_go = False
+right_go = False
+space_go = False
+
+bomb_list = []
+k = 0
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -53,12 +62,56 @@ while SB:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             SB = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_go = True
+            elif event.key == pygame.K_RIGHT:
+                right_go = True
+            elif event.key == pygame.K_SPACE:
+                space_go = True
+                k = 0
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left_go = False
+            elif event.key == pygame.K_RIGHT:
+                right_go = False
+            elif event.key == pygame.K_SPACE:
+                space_go = False
 
     # 4-3. 입력, 시간에 따른 변화
+    if left_go:
+        jet.x -= jet.move
+        if jet.x <= 0:
+            jet.x = 0
+    elif right_go:
+        jet.x += jet.move
+        if jet.x >= size[0] - jet.sx:
+            jet.x = size[0] - jet.sx
+
+    if space_go and k % 5 == 0:
+        bomb = obj()
+        bomb.put_img("./image/bomb.svg")
+        bomb.change_size(5, 15)
+        bomb.x = round(jet.x + (jet.sx - bomb.sx) / 2)
+        bomb.y = jet.y
+        bomb.move = 15
+        bomb_list.append(bomb)
+    k += 1
+    d_list = []
+    for i in range(len(bomb_list)):
+        b = bomb_list[i]
+        b.y -= b.move
+        if b.y <= -b.sy:
+            d_list.append(i)
+    d_list.reverse()
+    for d in d_list:
+        del bomb_list[d]
 
     # 4-4. 그리기
     screen.fill(black)
     jet.show()
+    for b in bomb_list:
+        b.show()
 
     # 4-5. 업데이트
     pygame.display.flip()
